081748e9c0d3145d0d7a1a7a2471b5797b177ffb
misakisoeda <misaki.soeda.29@gmail.com>
2023-11-30 21:55:16 +0900

    feat: 実行テストをしたファイル

---
+++ typescript-lesson/car.ts
@@ -0,0 +1,7 @@
+// class Car {}
+
+
+// $ tsc car.ts デフォルトはES3にコンパイルされることを確認
+// $ tsc car.ts --target ES2015 これで変換してくれる
+// $ tsc car.ts --target ES6 これで変換してくれる
+// https://compat-table.github.io/compat-table/es6/
