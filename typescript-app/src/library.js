f79047412b8b9e5c317dd4ecc42f3ebf2a926a14
misakisoeda <misaki.soeda.29@gmail.com>
2023-12-18 23:41:35 +0900

    feat: namespaceについて

--- typescript-app/src/library.js
+++ typescript-app/src/library.js
@@ -1,25 +1,19 @@
-import axios from "axios";
-axios.get('https://fooapi.com')
-
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = require("axios");
+axios_1.default.get('https://fooapi.com');
 // 誰かがd.tsを作っていないか調べる（googleで）
 // node_modules/@types/node の中に入れる
 // $ npm install --save-dev @types/lodash
 // $ npm uninstall --save-dev @types/lodash でアンインストールできる
-
 // import _ from 'lodash';  // <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js"></script> CDNで読み込んだのでコメントアウト
 // _.shuffle([1, 2, 3, 4]);
-
 // CDNで読み込んだ場合は、tsconfig.jsonの"noEmitOnError"も削除する
 // console.log(_.shuffle([1, 2, 3, 4]));
-
-namespace myApp {
-    const hello = 'hello in namespace';
-    export const name = 'Quill';
-    export interface Nameable {
-        name: string;
-    }
-}
+var myApp;
+(function (myApp) {
+    var hello = 'hello in namespace';
+    myApp.name = 'Quill';
+})(myApp || (myApp = {}));
 // const hello = 'hello in global'
-const hello = myApp.name;
-let nameable: myApp.Nameable;   // ←「.」が使われているということは、namespaceが使われている。覚えておいた方が良い
+var hello = myApp.name;
--- typescript-app/src/lodash.d.ts
+++ typescript-app/src/lodash.d.ts
@@ -1,13 +1,21 @@
+// ↓npmでインストールしたライブラリを使う
 declare module 'lodash' {
     export function shuffle<T>(arr: T[]): T[]
 }
 
-interface Lodash {
-    shuffle<T>(arr: T[]): T[]
-}
 // ↓interface を使ってみる
+// interface Lodash {
+//     shuffle<T>(arr: T[]): T[]
+// }
+
 // declare const _: {  // 今回、　_ はオブジェクト
 //     shuffle<T>(arr: T[]): T[]
 // }
 // ↓interface を使ってみる
-declare const _: Lodash
+// declare const _: Lodash
+
+// ↓namespaceを使う
+declare namespace _ {
+    function shuffle<T>(arr: T[]): T[]
+}
+
